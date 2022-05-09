f73cab7f19b0e03d1228d3023c78ff8adfbd02a8
wangwei <412390898@qq.com>
2022-05-09 21:24:31 +0800

    修改换行符

--- src/test/java/com/example/testemail/TestEmailApplicationTests.java
+++ src/test/java/com/example/testemail/TestEmailApplicationTests.java
@@ -1,13 +1,1 @@
-package com.example.testemail;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TestEmailApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+package com.example.testemail;import org.junit.jupiter.api.Test;import org.springframework.boot.test.context.SpringBootTest;@SpringBootTestclass TestEmailApplicationTests {    @Test    void contextLoads() {    }}
